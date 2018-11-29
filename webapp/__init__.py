8cd0658c25021e5637185e7d1b38f6c3204dbcb5
lichengbo <lichengbo@huishoubao.com.cn>
2018-11-29 16:42:56 +0800

    add flask framework

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,49 @@
 # -*- coding:utf-8 -*-
+import traceback
+import pkgutil
+import importlib
+from functools import wraps
+from flask import request, Response, Blueprint
+from voluptuous import MultipleInvalid
+from werkzeug import exceptions as wkerr
+
+from util import jsonify
+from config.logger import logger as LOGGER
+from myException import webappException as Err
+
+
+def route(app, *args, **kwargs):
+    def decorator(func):
+        @app.route(*args, **kwargs)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                rv = func(*args, **kwargs)
+            except (wkerr.BadRequest, MultipleInvalid) as e:
+                LOGGER.exception_log(str(e))
+                return jsonify(request.path, has_error=True, data=Err.ErrArgs)
+            except Err.ServiceError as e:
+                if e.code == 1001:
+                    return Response('relogin', status=599)
+                return jsonify(request.path, has_error=True, data=e)
+            except Exception:
+                LOGGER.exception_log(traceback.format_exc())
+                return jsonify(request.path, has_error=True, data=Err.ErrInternal)
+
+            return jsonify(request.path, data=rv)
+
+        return func
+    return decorator
+
+
+def register_blueprints(app):
+    for _, name, _ in pkgutil.iter_modules(__path__):
+        m = importlib.import_module("%s.%s" % (__name__, name))
+        for item in dir(m):
+            item = getattr(m, item)
+            if isinstance(item, Blueprint):
+                app.register_blueprint(item)
+    return app
 
 
 
